f5d94b13b83eb9912e2130d827a63ea1ffb9d2c0
jortimar <javier.orti-marina@altran.com>
2024-02-10 21:03:08 +0100

    feat:css app.css

--- src/Controller/Client/FrontEndController.php
+++ src/Controller/Client/FrontEndController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Client;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,6 +16,20 @@ class FrontEndController extends AbstractController
     {
         return $this->render('client/index.html.twig', [
             'controller_name' => 'FrontEndController',
+        ]);
+    }
+
+    #[Route(path: '/registrer', name: 'new_user')]
+    public function newUserAction(Request $request): Response
+    {
+
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('new_user');
+        }
+        return $this->render('client/new_user.html.twig', [
+            'form'=>$form->createView()
         ]);
     }
 }
